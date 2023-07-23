36dd2936f6e25345f02243eedbee736ca7faf856
bodzash <bozzaylaszlo4@gmail.com>
2023-07-23 11:39:05 +0200

    chore: fixed dsadasdasdas

--- zcompileTexture.bat
+++ zcompileTexture.bat
@@ -5,15 +5,15 @@ REM Regular model textures shold use ETC1 or the other one
 REM texturec -f ./Assets/Jack/HandsomeJack_Diff.png -o ./build/Debug/Jack/HandsomeJack_Diff.dds --mips -t ETC1
 REM texturec -f ./Assets/Jack/HandsomeJackMask_Diff.png -o ./build/Debug/Jack/HandsomeJackMask_Diff.dds --mips -t ETC1
 
+texturec -f ./Assets/Angel/Vog_Dif.png -o ./build/Debug/Angel/Vog_Dif.dds --mips -t ETC1
+texturec -f ./Assets/Angel/Vog_Dif_s.png -o ./build/Debug/Angel/Vog_Dif_s.dds --mips -t ETC1
+
 REM Transparent textures should use RGBA8 or BGRA8
 REM texturec -f ./Assets/Grass.png -o ./build/Debug/Grass.dds --mips -t RGBA8
 
 REM Skybox textures should use NO mipmaps and RGB8 or another losless format
 REM texturec -f ./Assets/Skybox.png -o ./build/Debug/Skybox.dds --strip -t RGB8
 REM texturec -f ./Assets/SkyboxDay.png -o ./build/Debug/SkyboxDay.dds --strip -t RGB8
-
-texturec -f ./Assets/Angel/Vog_Dif.png -o ./build/Debug/Angel/Vog_Dif.dds --mips -t ETC1
-texturec -f ./Assets/Angel/Vog_Dif_s.png -o ./build/Debug/Angel/Vog_Dif_s.dds --mips -t ETC1
 
 @ECHO ON
 

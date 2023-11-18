ae324fe38a687ededa2c99c4e3edb4196c704da3
Laszlo Bozzay <bozzaylaszlo4@gmail.com>
2023-11-18 12:05:06 +0100

    dasdasd

--- Source/Animation.h
+++ Source/Animation.h
@@ -36,10 +36,7 @@ public:
     inline float GetTicksPerSecond() { return m_TicksPerSecond; }
     inline float GetDuration() { return m_Duration;}
     inline const AssimpNodeData& GetRootNode() { return m_RootNode; }
-    inline const std::map<std::string, BoneInfo>& GetBoneIDMap() 
-    { 
-        return m_BoneInfoMap;
-    }
+    inline const std::map<std::string, BoneInfo>& GetBoneIDMap() { return m_BoneInfoMap; }
 
 private:
     void ReadMissingBones(const aiAnimation* animation, Model& model);
